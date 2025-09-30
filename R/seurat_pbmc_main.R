733367af528849d8cf29c9894520f4eba55abb37
Olivia Scharfman <oliviascharfman@gmail.com>
2025-09-29 23:47:08 -0400

    Update main script

--- R/seurat_pbmc_main.R
+++ R/seurat_pbmc_main.R
@@ -42,7 +42,7 @@ library(sctransform)
 library(patchwork)
 
 # Load the PBMC dataset
-pbmc.data <- Read10X(data.dir = "/Users/gw/Downloads/pbmc3k_data/filtered_gene_bc_matrices/hg19/")
+pbmc.data <- Read10X(data.dir = "data/filtered_gene_bc_matrices/hg19")
 
 # Initialize the Seurat object with the raw (non-normalized data).
 pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
